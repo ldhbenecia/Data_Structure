a3f95a10f912336eea1eefba24606643f864557b
DongHyeok Lim <zx07050@naver.com>
2021-12-13 18:51:53 +0900

    [docs] queue 기본 파일 수정

--- 01_Array_List_Stack_Queue/queue_basic.py
+++ 01_Array_List_Stack_Queue/queue_basic.py
@@ -1,20 +1,28 @@
-# stack_queue.py 에 저장
 class Queue:
-	def __init__(self):
-		self.items = []	# 데이터 저장을 위한 리스트 준비
-	def enqueue(self, val):
-		self.items.append(val)
-	def dequeue(self):
-		try:	# pop할 아이템이 없으면
-			return self.items._____________ # 어떤 함수를 호출해야 할까?
-		except IndexError:	# indexError 발생
-			print("Queue is empty")
-	def front(self):
-		try:
-			return self.items[______]  # 어떤 값이 와야할까?
-		except IndexError:
-			print("Queue is empty")
-	def __len__(self):	# len()로 호출하면 stack의 item 수 반환
- 		return len(self.items)
-	def isEmpty(self):
-		return len(self)
+    def __init__(self):
+        self.item = []
+        self.front_index = 0
+        
+    def enqueue(self, val):
+        self.item.append(val)
+        
+    def dequeue(self):
+        if (self.front_index == len(self.item)): # 큐가 비어있음
+            print("Queue is empty!")
+            return None
+        else:
+            x = self.item[self.front_index]
+            self.front_index += 1
+            return x
+    
+    def isEmpty(self):
+        if (self.front_index == len(self.item)):
+            print("Queue is empty!")
+        else:
+            return len(self.item) - self.front_index
+        
+    def front(self): # 가장 왼쪽에 저장된 값을 삭제하지 않고 리턴
+        return self.item[self.front_index]
+    
+    def len(self):
+        return len(self.item) - self.front_index
