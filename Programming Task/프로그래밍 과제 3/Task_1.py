3b7a6226c3081373fabbfada3293844db45177a9
DongHyeok Lim <zx07050@naver.com>
2022-03-28 19:11:14 +0900

    Task_1 주석 처리

--- Programming Task/프로그래밍 과제 3/Task_1.py
+++ Programming Task/프로그래밍 과제 3/Task_1.py
@@ -25,23 +25,22 @@ class Stack:
   
 
 def Balance(s):
-  S = Stack()
-  openBracket = '({['
-  closeBracket = ')}]'
+  S = Stack() # 스택 준비
+  openBracket = '({[' # 여는 괄호
+  closeBracket = ')}]' # 닫는 괄호
   
-  for ch in s:
-    if ch in openBracket:
+  for ch in s: # 입력 문자열을 하나씩 읽으며 반복
+    if ch in openBracket: # 만약 여는 괄호면 스택에 push
       S.push(ch)
-    elif ch in closeBracket:
-      if S.isEmpty():
-        return 0
-      else:
-        openCh = S.pop()
+    elif ch in closeBracket: # 닫는 괄호이면
+      if S.isEmpty(): # 스택이 비어있을 경우(여는 괄호가 없을 경우)
+        return 0 # 0 출력
+      else: # 스택이 비어있지 않으면
+        openCh = S.pop() # pop() 변수 지정
         if (ch == ')' and openCh != '(') or (ch == '}' and openCh != '{') or (ch == ']' and openCh != '['):
-          return 0
+          return 0 # ch에 대응하는 여는 괄호가 아닐 경우 짝이 맞지 않으므로 0 출력
         
-        
-  if len(S) == 0:
+  if len(S) == 0: # 스택의 길이가 0일 경우 
     return 1
   else:
     return 0
