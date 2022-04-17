7fe3b774e9e55108d8304d5f0617ed92511c5d08
DongHyeok Lim <zx07050@naver.com>
2022-04-17 15:40:16 +0900

    우선순위 큐

---
+++ 01_Array_List_Stack_Queue/PriorityQueue.py
@@ -0,0 +1,36 @@
+# 우선순위 큐
+class PriorityQueue:
+  def __init__(self):
+    self.items = []
+    
+  def isEmpty(self):
+    return len(self.items) == 0
+  
+  def size(self):
+    return len(self.items)
+  
+  def clear(self):
+    self.items = []
+    
+  def enqueue(self, e):
+    self.items.append(e)
+    
+  def findMaxIndex(self):
+    if self.isEmpty():
+      return None
+    else:
+      highest = 0
+      for i in range(1, self.size()):
+        if self.items[i] > self.items[highest]:
+          highest = i
+      return highest
+    
+  def dequeue(self):
+    highest = self.findMaxIndex()
+    if highest is not None:
+      return self.items.pop(highest)
+    
+  def peek(self):
+    highest = self.findMaxIndex()
+    if highest is not None:
+      return self.items[highest]
