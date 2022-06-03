1cd98d4b467c7add6102f9979b2bcc862ca3fdbc
DongHyeok Lim <zx07050@naver.com>
2022-06-03 17:59:26 +0900

    정렬 알고리즘
    선택, 삽입, 버블 정렬

---
+++ 04_heap_sort/Sort(selection, insertion, bubble).py
@@ -0,0 +1,65 @@
+# 선택 정렬
+# 시간 복잡도 = O(n^2)
+
+arr = []
+
+def selection_sort(arr):
+  n = len(arr)
+  
+  for i in range(n-1):
+    least = i
+    for j in range(i+1, n):
+      if (arr[j] < arr[least]):
+        least = j
+    arr[i], arr[least] = arr[least], arr[i]
+    
+
+# 삽입 정렬
+# 시간 복잡도 = O(n^2)
+
+# 최적화 된 삽입정렬 코드
+def insertion_sort(arr): # swap 작업 없이 단순히 값들을 shift
+  n = len(arr)
+  for i in range(1, n):
+    key = arr[i]
+    j = i-1
+    while j >= 0 and arr[j] > key:
+      arr[j+1] = arr[j]
+      j -= 1
+    arr[j+1] = key
+    
+# 일반적인 삽입 정렬 코드
+def insertion_sort2(arr):
+  n = len(arr)
+  for end in range(1, n):
+    for i in range(end, 0, -1):
+      if arr[i-1] > arr[i]:
+        arr[i-1], arr[i] = arr[i], arr[i-1]
+        
+        
+# 버블 정렬
+# 시간 복잡도 = O(n^2)
+
+# 최적화 -> 이전 패스에서 swap이 한 번도 일어나지 않았다면
+# 정렬되지 않는 값이 하나도 없었다고 간주. 이럴 경우, 이후 패스를 수행하지 않아도 됨
+def bubble_sort(arr):
+  n = len(arr)
+  
+  for i in range(n-1, 0, -1):
+    bChanged = False
+    for j in range(i):
+      if (arr[j] > arr[j+1]):
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+        bChanged = True
+  
+    if not bChanged: break;
+    
+  
+# 일반적인 버블 정렬 코드
+def bubble_sort2(arr):
+  n = len(arr)
+  
+  for i in range(n -1 , 0, -1):
+    for j in range(i):
+      if arr[j] > arr[j+1]:
+        arr[j], arr[j+1] = arr[j+1], arr[j]
